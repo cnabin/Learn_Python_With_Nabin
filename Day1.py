219937dabac50c901bded757011763eb94b7557c
cnabin <151549540+cnabin@users.noreply.github.com>
2024-03-19 21:13:22 +0545

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,31 @@
+# print("Hello World!")
+# Ctrl + `
+
+# name='Nabin'
+# Name='Mindriser'
+
+# print(name)
+# print(Name)
+
+# a=0
+# b=1
+
+# # temp=a
+# # a=b
+# # b=temp
+
+# a,b=b,a
+
+# print(a,b)
+""" alt + shift + A """
+
+seat1="nabin"
+seat2="student2"
+
+teacher=seat1
+seat1=seat2
+seat2=teacher
+
+print(seat1,seat2)
+
+
